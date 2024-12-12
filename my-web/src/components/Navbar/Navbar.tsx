2438af29c6b4ec5751042819e9fbe84e21307d58
OliOSZ <146973447+OliOSZ@users.noreply.github.com>
2024-12-12 17:19:11 +0100

    navbar style and html

--- my-web/src/components/Navbar/Navbar.tsx
+++ my-web/src/components/Navbar/Navbar.tsx
@@ -1,43 +1,74 @@
 import { Link, useLocation } from "react-router-dom";
-import "./Navbar.css"
+import './Navbar.css';
 
 const Navbar = () => {
-const location = useLocation();
-
+  const location = useLocation();
 
   return (
     <nav className="Navbar">
       <ul className="NavbarList">
-        <li>
-          <Link className={`NavbarLink ${location.pathname === "/" ? "active" : ""}`}
-            to="/">FriMedYoga</Link>
+        <li className="NavbarBrand">
+          <Link to="/" className="NavbarLink BrandLink">FriMedYoga</Link>
         </li>
-        <li>
-          <Link className={`NavbarLink ${location.pathname === "/BlogPage" ? "active" : ""}`}
-            to="/BlogPage">Blog</Link>
-        </li>
-        <li>
-          <Link className={`NavbarLink ${location.pathname === "/KursPage" ? "active" : ""}`}
-            to="/KursPage">Kurs</Link>
-        </li>
-        <li>
-          <Link className={`NavbarLink ${location.pathname === "/EventPage" ? "active" : ""}`}
-            to="/EventPage">Eventer</Link>
-        </li>
-
-        <li>
-          <Link  className={`NavbarLink ${location.pathname === "/FAQ" ? "active" : ""}`}
-            to="/FAQ">Kontakt</Link>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/FriMedYoga/" target="_blank">
-            <i className='bx bxl-facebook-square bx-md' id="facebookLogo"></i>
-          </a>
-        </li>
-        
+        <div className="NavCenter">
+          <li>
+            <Link
+              className={`NavbarLink ${location.pathname === "/BlogPage" ? "active" : ""}`}
+              to="/BlogPage"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`NavbarLink ${location.pathname === "/KursPage" ? "active" : ""}`}
+              to="/KursPage"
+            >
+              Kurs
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`NavbarLink ${location.pathname === "/EventPage" ? "active" : ""}`}
+              to="/EventPage"
+            >
+              Eventer
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`NavbarLink ${location.pathname === "/FAQ" ? "active" : ""}`}
+              to="/FAQ"
+            >
+              Kontakt
+            </Link>
+          </li>
+        </div>
+        <div className="NavIcons">
+          <li>
+            <a href="https://www.facebook.com/FriMedYoga/" target="_blank" rel="noopener noreferrer">
+              <i className="bx bxl-facebook-square bx-md"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/frimedyoga/" target="_blank" rel="noopener noreferrer">
+              <i className="bx bxl-instagram-alt bx-md"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/FriMedYoga/" target="_blank" rel="noopener noreferrer">
+              <i className="bx bxl-basket bx-md"></i>
+            </a>
+          </li>
+          <li>
+            <a href="/profile" className="ProfileLink">
+              <i className="bx bxs-user-circle bx-md"></i>
+            </a>
+          </li>
+        </div>
       </ul>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
